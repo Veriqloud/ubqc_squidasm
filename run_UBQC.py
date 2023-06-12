33798f43c44daf56780684baf990db61204a09fb
nininaceur <111692547+nininaceur@users.noreply.github.com>
2023-06-12 16:05:01 +0200

    Update run_UBQC.py

--- run_UBQC.py
+++ run_UBQC.py
@@ -4,19 +4,21 @@ from squidasm.run.stack.config import StackNetworkConfig
 from squidasm.run.stack.run import run
 import netsquid
 
-
-
+# Loading network configuration
 cfg = StackNetworkConfig.from_file("config.yaml")
 
-
+# Loading the server's and client's instructions
 alice_program = AliceProgram()
 bob_program = BobProgram()
 
+# Settings for subroutine allowing us to display qubits
 netsquid.set_qstate_formalism(netsquid.QFormalism.DM)
+
+# Initialize array with results. Change here if the result of another circuit than circuit 4 (expected outcome: [1,1,0] should be confirmed. Change num_times to change number of iterations.
 meas = []
 meas.append(run(config=cfg,
     programs={"Alice": alice_program, "Bob": bob_program},
-    num_times=1))
+    num_times=50))
     
 results = meas[0][0]
 counter = 0
