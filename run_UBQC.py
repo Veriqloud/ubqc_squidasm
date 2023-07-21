0aa8b39a0d9b877865317c6d69912cd77dc24573
Younes Naceur <111692547+nininaceur@users.noreply.github.com>
2023-07-21 17:34:17 +0200

    Update run_UBQC.py

--- run_UBQC.py
+++ run_UBQC.py
@@ -3,22 +3,31 @@ from UBQC_server import BobProgram
 from squidasm.run.stack.config import StackNetworkConfig
 from squidasm.run.stack.run import run
 import netsquid
+netsquid.set_qstate_formalism(netsquid.QFormalism.KET)
 
 # Here: Choose noise configuration. Can choose between the default configuration (config_default.yaml), a perfect config (config_perfect)
 # and a customized noise configuration (config_noise.yaml). Standard is perfect configuration.
 
 cfg = StackNetworkConfig.from_file("config_perfect.yaml")
 
+# Load client's and server's instructions
+
 alice_program = AliceProgram()
 bob_program = BobProgram()
 
-netsquid.set_qstate_formalism(netsquid.QFormalism.KET)
+# Initialize array with simulation results
+
 meas = []
+
+# Run the simulation. Number of iterations can be changed here.
+
 meas.append(run(config=cfg,
     programs={"Alice": alice_program, "Bob": bob_program},
     num_times=100))
     
 results = meas[0][0]
+
+# Estimating the success rate by comparing the measurement results with the expected results (found for test circuits in circuits_qasm.py)
 
 counter = 0
 
