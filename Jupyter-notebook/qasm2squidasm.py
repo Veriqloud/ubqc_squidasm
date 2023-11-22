ba592334a175231f071396842b6914619d30457d
ctliao <liao.chinte@gmail.com>
2023-11-22 18:01:46 +0100

    first example done

--- Jupyter-notebook/qasm2squidasm.py
+++ Jupyter-notebook/qasm2squidasm.py
@@ -7,41 +7,43 @@ from qiskit.qasm3 import dump
 from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
 from netqasm.sdk.qubit import Qubit
 
-# Circuit 5, Expected outcome: [1,1]
-q5 = QuantumRegister(2)
-c5 = ClassicalRegister(2)
-qc5 = QuantumCircuit(q5, c5)
-qc5.h(q5[0])
-qc5.z(q5[1])
-qc5.h(q5[1])
-qc5.cx(q5[1],q5[0])
 
 
-def text2squidasm(num_qubits,operators,connection):
-    print("text2squidasm")
-    myqubitList = [] 
-    for q in range(num_qubits):
-        myqubitList.append(Qubit(connection))
+
+def text2squidasm(myqubitList,operators):
+    #print("text2squidasm")
+    for q in myqubitList:
+        q.H()  
 
     for line in operators:
-        print(line)
-        
-        if line[0] == 'h':                    
+        print(f"processing...{line}")
+        if line[0] == 'h':  
+            #print(f"applyed h in qubit {line[1]}")                  
             myqubitList[int(line[1])].H()     
-            print(f"applyed h in qubit {line[1]}")
+            
         elif line[0] == 'x':  
-            print(f"applyed x in qubit {line[1]}")                  
+            #print(f"applyed x in qubit {line[1]}")      
             myqubitList[int(line[1])].X() 
+
         elif line[0] == 'z':  
-            print(f"applyed z in qubit {line[1]}")                
+            #print(f"applyed z in qubit {line[1]}")                
             myqubitList[int(line[1])].Z() 
+        
+        elif line[0] == 'cx' :   # control case
+            #print(f"applyed cnot in qubit {line[1],line[2]}") 
+            myqubitList[int(line[1])].cnot(myqubitList[int(line[2])])
+
+        elif line[0] == 'rx': # rotation case
+            #print(f"applyed cnot in qubit {line[1],line[2]}") 
+            myqubitList[int(line[1])].cnot(myqubitList[int(line[2])])
+        
         else:
             pass
     
     # measure all qubits
     res = []
-    for i in range(num_qubits):
-        res.append(myqubitList[i].measure())
+    for q in myqubitList:
+        res.append(q.measure())
         
     
     #print(f"res:{res}")
@@ -51,17 +53,17 @@ def text2squidasm(num_qubits,operators,connection):
 
 
 def qasm2text(qcircuit):
-    print("qasm2text")
+    #print("qasm2text")
     # make .qasm file for easier parsing
-    iostream = open("qasm2squidamsTest.qasm", "w", encoding="utf-8")
+    iostream = open("tempCircuit.qasm", "w", encoding="utf-8")
     dump(qcircuit,iostream)
     iostream.close()
 
     # read from .qasm file and parse it
-    myqasm = open("qasm2squidamsTest.qasm", "r", encoding="utf-8")  
+    myqasm = open("tempCircuit.qasm", "r", encoding="utf-8")  
     mytext = myqasm.readlines()
     text_size = len(mytext)
-    print(mytext)
+    #print(mytext)
 
     # parse text:
     num_qubits = int(mytext[3][6]) #fix location of num_qubits 
@@ -69,7 +71,11 @@ def qasm2text(qcircuit):
 
     operators = []
     for i in range(5,text_size):
-        if mytext[i][0] == 'c':
+        if mytext[i][0] == 'c': # control case
+            operators.append([mytext[i][0]+mytext[i][1],mytext[i][6],mytext[i][13]])
+        if mytext[i][0] == 'r': # rotation case
+            # extract parameters
+            #operators.append()
             pass
         else:
             operators.append([mytext[i][0],mytext[i][5]]) #fix location of operators
@@ -83,9 +89,10 @@ def qasm2text(qcircuit):
 class CircuitProgram(Program):
     PEER_NAME = "Alice"
     
-    def __init__(self,maxQubits: int):
+    def __init__(self,maxQubits: int, qcircuit):
         self.maxQubits = maxQubits
         self.res = []
+        self.qcircuit = qcircuit
 
     @property
     def meta(self) -> ProgramMeta:
@@ -99,30 +106,70 @@ class CircuitProgram(Program):
     def run(self, context: ProgramContext):
 
         myConnection = context.connection
-        num_qubits,operators = qasm2text(qc5)
-        self.res = text2squidasm(num_qubits,operators,myConnection)
+        num_qubits,operators = qasm2text(self.qcircuit)
+
+        # initialize qubits
+        myqubitList = [] 
+        for q in range(num_qubits):
+            myqubitList.append(Qubit(myConnection))
+
+        self.res = text2squidasm(myqubitList,operators)
         
-        #yield from connection.flush()
+        yield from myConnection.flush()
+        self.res=[int(i) for i in self.res]
+        #print(f"Measurement result:{self.res}")
+        
 
         return {}
 
 
 def display(qcircuit):
     print(qcircuit.draw())
-    print(disassemble(qcircuit))
+    #print(disassemble(qcircuit))
 
 
 
 
 if __name__ == "__main__":
 
+    # Circuit 5, Expected outcome: [1,1]
+    q5 = QuantumRegister(2)
+    c5 = ClassicalRegister(2)
+    qc5 = QuantumCircuit(q5, c5)
+    qc5.h(q5[0])
+    qc5.z(q5[1])
+    qc5.h(q5[1])
+    qc5.cx(q5[1],q5[0])
+
+    # Circuit 14, Expected outcome: [1,1,1,1,0]
+    q14 = QuantumRegister(5)
+    c14 = ClassicalRegister(5)
+    qc14 = QuantumCircuit(q14, c14)
+    qc14.z(q14[0])
+    qc14.z(q14[1])
+    qc14.h(q14[0])
+    qc14.h(q14[1])
+    qc14.h(q14[2])
+    qc14.h(q14[3])
+    qc14.h(q14[4])
+    qc14.rx(128,q14[2])
+    qc14.cx(q14[1],q14[3])
+    
+
     cfg = StackNetworkConfig.from_file("config_perfect.yaml")
 
     maxQubits = 20
-    myCircuitProgram = CircuitProgram(maxQubits)
+    myCircuitProgram = CircuitProgram(maxQubits,qc14)
+    num_times = 1
+    resList = []
 
-    run(config=cfg,
-        programs={ "Bob": myCircuitProgram},
-        num_times=1)
+    for _ in range(num_times):
 
+        run(config=cfg,
+            programs={ "Bob": myCircuitProgram},
+            num_times=1)
+        
+        resList.append(myCircuitProgram.res)
+
+    print(f"Measurement result:{resList}")
     #print(f"Measurement result:{myCircuitProgram.res}")
