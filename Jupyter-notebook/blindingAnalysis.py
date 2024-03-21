b979a67298532a2e25767b7791ac816571d2a36a
Chin-Te Liao <liao.chinte@gmail.com>
2024-03-21 12:13:48 +0100

    update config and blinding analysis

--- Jupyter-notebook/blindingAnalysis.py
+++ Jupyter-notebook/blindingAnalysis.py
@@ -5,8 +5,6 @@ We are able to see the efficiency differnce among the two.
 '''
 from squidasm.run.stack.config import StackNetworkConfig
 from squidasm.run.stack.run import run
-from circuits_qasm import qasm_circs
-#from qiskit.qasm3 import load
 
 from qasm2squidasm import CircuitProgram
 from UBQC_client import AliceProgram, LoadType
@@ -69,19 +67,22 @@ def ubqcSim(path,configfile,maxQubits,num_times,exp_result):
 
     #print(f"resListTmp: {resListTmp}")
     # extract only the result
-    if(configfile!="config_noise.yaml" and configfile!="config_lossy_link.yaml" and configfile!="config_noise2.yaml" ):    
+    if(configfile!="config_noise_low.yaml" and configfile!="config_lossy_link.yaml" and configfile!="config_noise_high.yaml" ):    
         resList = resListTmp[0][0]
+        #print(f"~~resList:{resList}")
 
-    if(configfile=="config_noise.yaml" or configfile=="config_lossy_link.yaml" or configfile=="config_noise2.yaml" ):
+    if(configfile=="config_noise_low.yaml" or configfile=="config_lossy_link.yaml" or configfile=="config_noise_high.yaml" ):
         #print(f"resListTmp :{resListTmp}")
         results = [resListTmp[i][1][0][0] for i in range(num_times)]
         resList.append(results)
 
+        
 
         for i in range(len(results)):
             if(results[i] == exp_result):
                 correct_counter += 1
-    
+
+    #print(f"~resList:{resList}")
 
     return resList, correct_counter/num_times
 
@@ -89,14 +90,14 @@ def ubqcSim(path,configfile,maxQubits,num_times,exp_result):
 
 if __name__ == "__main__":
 
-    fileName = "tempCircuit.qasm"
-    Mynum_times = 40
-    exp_res = [1,1,1,1,0]
+    fileName = "testCircuit5.qasm"
+    Mynum_times = 200
+    exp_res = [1,1] #[1,1,1,1,0]
 
     normRateList = []
     ubqcRateList = []
-
-    configFile = "config_noise.yaml"  #config_perfect.yaml,  config_default.yaml
+    '''
+    configFile = "config_perfect.yaml"  #config_perfect.yaml,  config_default.yaml
 
     normalRes,correct_rate = normalSim(fileName,configFile
         ,maxQubits=20,num_times=Mynum_times,exp_result=exp_res)
@@ -107,47 +108,69 @@ if __name__ == "__main__":
 
     ubquRes, ubqc_correct_rate = ubqcSim(fileName,configFile
         ,maxQubits=20,num_times=Mynum_times,exp_result=exp_res)
-    #print(f"ubquRes :{ubquRes}")
-    #print(f"ubqc crrect Rate :{ubqc_correct_rate}")
+    print(f"ubquRes :{ubquRes}")
+    print(f"ubqc crrect Rate :{ubqc_correct_rate}")
     ubqcRateList.append(ubqc_correct_rate)
+    '''
+    #=============================================================================
+    configFile = "config_noise_low.yaml"  #config_perfect.yaml,  config_default.yaml
+
+    normalRes1,correct_rate1 = normalSim(fileName,configFile
+        ,maxQubits=20,num_times=Mynum_times,exp_result=exp_res)
+    #print(f"normalRes1 :{normalRes1}")
+    #print(f"normal crrect Rate1 :{correct_rate1}")
+    normRateList.append(correct_rate1)
+    
+
+    ubquRes1, ubqc_correct_rate1 = ubqcSim(fileName,configFile
+        ,maxQubits=20,num_times=Mynum_times,exp_result=exp_res)
+    #print(f"ubquRes1 :{ubquRes1}")
+    #print(f"ubqc crrect Rate1 :{ubqc_correct_rate1}")
+    ubqcRateList.append(ubqc_correct_rate1)
 
     #=========================================================================
     
-    configFile = "config_noise2.yaml"
+    configFile = "config_noise_high.yaml"
     normalRes2,correct_rate2 = normalSim(fileName,configFile
         ,maxQubits=20,num_times=Mynum_times,exp_result=exp_res)
-    #print(f"normalRes :{normalRes}")
+    '''
+    print(f"normalRes2 :{normalRes2}")
+    print(f"normal crrect Rate2 :{correct_rate2}")
+    '''
     normRateList.append(correct_rate2)
     
 
     ubquRes2, ubqc_correct_rate2 = ubqcSim(fileName,configFile
         ,maxQubits=20,num_times=Mynum_times,exp_result=exp_res)
-    #print(f"ubquRes :{ubquRes}")
+    '''
+    print(f"ubquRes2 :{ubquRes2}")
+    print(f"ubqc crrect Rate2 :{ubqc_correct_rate2}")
+    '''
     ubqcRateList.append(ubqc_correct_rate2)
 
     
     #=========================================================================
     print(f"normRateList:{normRateList}")
     print(f"ubqcRateList:{ubqcRateList}")
-
+    #'perfect',
     df = pd.DataFrame({ 
-    'Setting': ['noise level1', 'noise level2'], 
+    'Hardware configuration': ['low noise', 'high noise'], 
     'nomal circuit': normRateList, 
     'UBQC circuit': ubqcRateList 
     }) # requires the same size in list
 
 
-    df.plot(x= 'Setting' ,y=['nomal circuit','UBQC circuit'], kind="bar")  #"Noise case", "Loss case"
+    df.plot(x= 'Hardware configuration' ,y=['nomal circuit','UBQC circuit'], kind="bar")  #"Noise case", "Loss case"
 
     plt.xticks(rotation='horizontal')
     plt.tight_layout()
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9 ) #bottom=0.8
 
-    plt.title('UBQC algorithm benchmarking on Circuit 14 ')
+    plt.title('UBQC algorithm benchmarking on Circuit 5 ')
     plt.yticks(np.arange(0.0, 1.0, 0.1))
     plt.ylabel('successful rate')
 
-    plt.savefig('plotTest9.png',bbox_inches='tight')
+    plt.savefig('plotTest16.png',bbox_inches='tight')
     plt.show()
 
 
